39699da6f72176a060227571b194d2c0c2be7627
Ziya507 <fziya515@gmail.com>
2025-03-07 11:08:39 +0530

    Add files via upload

---
+++ ziya/ziya/program5.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float num1,num2,product;
+printf("enter two floating-point numbers:");
+scanf("%f%f",&num1,&num2);
+product=num1*num2;
+printf("product:%f\n",product);
+return 0;
+}
+
